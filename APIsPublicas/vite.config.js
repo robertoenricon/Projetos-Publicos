578c511bc9ea2f4197684298929b91669f35b375
Roberto Enrico <robertoenrico_@hotmail.com>
2026-04-03 00:43:20 -0300

    passando a nova arquitetura para o vite

--- APIsPublicas/vite.config.js
+++ APIsPublicas/vite.config.js
@@ -5,20 +5,13 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/pages/dashboard/app.js',
+                'resources/js/pages/sabesp/app.js',
+            ],
             refresh: true,
         }),
         vue(),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: 'localhost',
-        },
-        watch: {
-            usePolling: true,
-        },
-    },
 });
